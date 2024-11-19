0a8f4e05ee8c69c0ea294176f6d984bd63b374cc
rahul-b-p <rahulzantoz10@gmail.com>
2024-11-19 16:51:02 +0530

    completed modules learing

---
+++ src/os.ts
@@ -0,0 +1,16 @@
+import { log } from 'console'
+import os from 'os'
+
+const totalMemory:number = os.totalmem()       //Returns the total memory in bytes.
+const freeMemory:number = os.freemem()        //Returns the available memory in bytes.
+
+log(totalMemory)
+log(freeMemory)
+
+log(os.arch())          //Returns the CPU architecture.
+log(os.cpus())         //Returns information about each CPU/core.
+log(os.platform())    //Returns the platform of the operating system.
+log(os.uptime())     //Returns the system uptime in seconds.
+
+console.log(`Home Directory: ${os.homedir()}`);
+console.log(`Temporary Directory: ${os.tmpdir()}`);
